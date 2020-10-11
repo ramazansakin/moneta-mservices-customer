64049ed16183d6ec2bf30fd0a7cb21a4a42d0e60
Ramazan Sakin <ramazansakin63@gmail.com>
2020-10-11 08:26:07 +0300

    Schema initialization and schema def impl

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,9 @@
 -- Table Schemas
-DROP TABLE IF EXISTS customer;
-
 CREATE TABLE IF NOT EXISTS customer(
-    id INT AUTO_INCREMENT  PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     lastname VARCHAR(25) NOT NULL,
     tckn VARCHAR(15) NOT NULL,
-    salary DOUBLE DEFAULT 0,
+    salary REAL DEFAULT 0,
     phone VARCHAR(15)
 );
